7e4f6b3621905991bf495f41868c597738e96e68
GAMA <a.mugisha1@alustudent.com>
2025-06-11 12:07:39 +0200

    Update insert_to_db.py

--- insert_to_db.py
+++ insert_to_db.py
@@ -1,1 +1,15 @@
+import sqlite3
+
+def insert_transactions(transactions):
+    conn = sqlite3.connect('momo.db')
+    cursor = conn.cursor()
+
+    for tx in transactions:
+        cursor.execute('''
+            INSERT INTO transactions (type, receiver, amount, date, raw_body)
+            VALUES (?, ?, ?, ?, ?)
+        ''', (tx.get("type"), tx.get("receiver"), tx.get("amount"), tx.get("date"), tx.get("raw_body")))
+
+    conn.commit()
+    conn.close()
 
