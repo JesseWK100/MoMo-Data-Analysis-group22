182f051046fac559f07b29e0434f9c154c40efc3
GAMA <a.mugisha1@alustudent.com>
2025-06-11 12:06:25 +0200

    Update init_db.py

--- init_db.py
+++ init_db.py
@@ -1,1 +1,22 @@
+import sqlite3
 
+def create_db():
+    conn = sqlite3.connect('momo.db')
+    cursor = conn.cursor()
+
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS transactions (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            type TEXT,
+            receiver TEXT,
+            amount INTEGER,
+            date TEXT,
+            raw_body TEXT
+        )
+    ''')
+
+    conn.commit()
+    conn.close()
+
+if __name__ == '__main__':
+    create_db()
