a07ecc2a522da16e307ad15f1c0c62899755291d
c-hirwa <c.hirwa@alustudent.com>
2025-06-14 22:17:03 +0200

    serve

--- server.py
+++ server.py
@@ -1,8 +1,8 @@
 from app import create_app
-from flask_cors import CORS  # ✅ Add this
+from flask_cors import CORS
 
 app = create_app()
-CORS(app)  # ✅ Enable CORS for your Flask app
+CORS(app)  # Enables CORS for all routes
 
 if __name__ == "__main__":
     app.run(debug=True)
