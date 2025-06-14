e26b9faa1809b932cf89e13aea66083dc9c28e96
c-hirwa <c.hirwa@alustudent.com>
2025-06-14 22:02:06 +0200

    server updated

--- server.py
+++ server.py
@@ -4,3 +4,9 @@ app = create_app()
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+    const cors = require('cors');
+const express = require('express');
+const app = express();
+
+app.use(cors()); // Enable CORS for all routes
